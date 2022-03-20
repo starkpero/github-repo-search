cdf7a5553e2728cb8a9e8f93bdf35aba8e139907
starkpero <anishgupta0066@gmail.com>
2022-03-20 21:22:15 +0530

    completed profile card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { GithubProfileComponent } from '../components/github-profile/github-profile.component';
+import { GithubReposComponent } from '../components/github-repos/github-repos.component';
+import { GithubProfileCardComponent } from '../components/github-profile-card/github-profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    GithubProfileComponent,
+    GithubReposComponent,
+    GithubProfileCardComponent
   ],
   imports: [
     FormsModule,
