10ade5f923bc3de53b175f61c362e816f0a21ecf
starkpero <anishgupta0066@gmail.com>
2022-03-21 10:14:11 +0530

    Ui fixes and logic fixes in repo listing component

--- src/components/github-repos/github-repos.component.ts
+++ src/components/github-repos/github-repos.component.ts
@@ -7,12 +7,20 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GithubReposComponent implements OnInit {
 
-  @Input() githubRepos:any;
-  @Input() totalRepos:number;
-  @Input() page:number;
+  // @Input() githubRepos:any;
+  // @Input() totalRepos:number;
+  // @Input() page:number;
+  @Input() name: string = '';
+  @Input() description: string = '';
+  @Input() language: string = '';
+  @Input() repositoryLink: string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  redirect = () => {
+    window.open(this.repositoryLink, '_blank');
+  };
 
 }
